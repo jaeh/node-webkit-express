efdccdc53987450690e5d4f8042fc551ec47b873
Jascha Ehrenreich <jascha@jaeh.at>
2014-12-05 02:06:17 +0100

    removed middleware

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
  * Module dependencies.
  */
 var http = require('http')
-  , express = require('express')
   , path = require('path')
+  , express = require('express')
   , routes = require(path.join(process.cwd(), 'routes', 'index.js'))
-  , app = express();
+  , app = express()
+;
 
 var options = {
   host: 'localhost',
@@ -16,29 +17,19 @@ var options = {
 
 //check if server is already running
 http.get(options, function(res) {
-  console.log('server runs already, redirecting to localhost');
+  console.log('server is running, redirecting to localhost');
   if (window.location.href.indexOf('localhost') < 0) { 
     window.location = 'http://localhost:' + app.get('port');
   }
 }).on('error', function(e) {
   //server is not yet running
-  
+
   // all environments
   app.set('port', process.env.PORT || 2323);
   app.set('views', process.cwd() + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
   app.use(require('stylus').middleware(path.join(process.cwd(), 'public')));
   app.use(express.static(path.join(process.cwd(), 'public')));
-
-  // development only
-  if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-  }
 
   app.get('/', routes.index);
   app.get('/test', routes.test);
